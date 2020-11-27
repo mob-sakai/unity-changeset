e630398129382077efb39388ad57691dd65084b4
mob-sakai <sakai861104@gmail.com>
2020-11-27 18:02:40 +0900

    feat: get unity changeset (beta)

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,9 @@ export const getUnityChangeset = async (version: string): Promise<UnityChangeset
   switch (match?.[1] as string) {
     case 'f':
       return await getArchivedChangeset(version);
+    case 'a':
+    case 'b':
+      return await getBetaChangeset(version)
     default:
       throw Error('The given version was not supported')
   }
@@ -22,4 +25,13 @@ export const getUnityChangeset = async (version: string): Promise<UnityChangeset
 export const getArchivedChangeset = async (version: string): Promise<UnityChangeset> => {
   const versions = await scrapeArchivedChangesets();
   return versions.filter(c => c.version === version)[0];
+};
+
+export const getBetaChangeset = async (version: string): Promise<UnityChangeset> => {
+  const document = await getDocumentFromUrl(`https://unity3d.com/unity/beta/${version}`);
+
+  return Array.from(document.querySelectorAll('a[href]'))
+    .map(a => a.getAttribute('href') as string)
+    .filter(href => UnityChangeset.isValid(href))
+    .map(href => UnityChangeset.createFromHref(href))[0];
 };
