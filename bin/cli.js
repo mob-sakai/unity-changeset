c7929c60bb6bc106fc1d9fff6f7371bcf6a46e28
mob-sakai <sakai861104@gmail.com>
2020-11-28 16:01:42 +0900

    change(cli):  `list` is now a subcommand

--- bin/cli.js
+++ bin/cli.js
@@ -3,39 +3,39 @@
 const { getUnityChangeset, scrapeArchivedChangesets, scrapeBetaChangesets } = require("../dist/index");
 const cli = require('cac')();
 
-cli.command('[version]', 'Get a changeset for specific version')
-  .option('-l, --list', 'List changesets')
-  .option('-b, --list-beta', 'List beta changesets')
-  .example('unity-changeset 2020.2.14f1')
-  .example('unity-changeset --list')
-  .example('unity-changeset --list-beta')
-  .example('unity-changeset --list --list-beta')
+cli.command('<version>', 'Get a changeset for specific version')
   .action((version, options) => (async () => {
-    if (version) {
-      try {
-        var changeset = await getUnityChangeset(version);
-        console.log(changeset.changeset);
-      } catch {
-        console.error('The given version was not found.');
-        process.exit(1);
-      }
-    }
-    else {
-      if (process.argv.length < 3) {
-        cli.outputHelp();
-      }
-      if (options.list) {
-        var changesets = await scrapeArchivedChangesets();
-        console.log(changesets.map(c => c.toString()).join('\n'));
-      }
-      if (options.listBeta) {
-        var changesets = await scrapeBetaChangesets();
-        console.log(changesets.map(c => c.toString()).join('\n'));
-      }
+    try {
+      var changeset = await getUnityChangeset(version);
+      console.log(changeset.changeset);
+    } catch {
+      console.error('The given version was not found.');
+      process.exit(1);
     }
   })());
 
-cli.help();
-cli.version('1.0.0');
+cli.command('list', 'List changesets')
+  .option('-b, --beta', 'List beta changesets')
+  .action(options => (async () => {
+    var results = options.listBeta
+      ? await scrapeArchivedChangesets()
+      : await scrapeArchivedChangesets();
+
+    console.log(changesets.map(c => c.toString()).join('\n'));
+  })());
+
+cli
+  .usage('unity-changeset <command> [options]')
+  .example('unity-changeset 2020.1.15f1')
+  .example('unity-changeset 2021.1.0a7')
+  .example('unity-changeset list')
+  .example('unity-changeset list --beta')
+  .help()
+  .version('1.1.0');
+
+if (process.argv.length < 3) {
+  cli.outputHelp(true);
+  process.exit(1);
+}
 
 cli.parse();
