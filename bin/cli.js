2a42019a43bb0a3f93a74b920c5b2f4b8e5df30f
mob-sakai <sakai861104@gmail.com>
2020-11-30 00:13:10 +0900

    feat(cli): add `--min` `--max` option for `list`
    Filter by min/max version

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,17 @@
 const { getUnityChangeset, scrapeArchivedChangesets, scrapeBetaChangesets } = require("../dist/index");
 const cli = require('cac')();
 
+toNumber = function (version) {
+  const match = version.toString().match(/^(\d+)\.*(\d*)\.*(\d*)(\w*)(\d*)$/);
+  if (match === null) return 0;
+
+  return parseInt(match[1] || '0') * 100 * 100 * 100 * 100
+    + parseInt(match[2] || '0') * 100 * 100 * 100
+    + parseInt(match[3] || '0') * 100 * 100
+    + ((match[4] || 'a').toUpperCase().charCodeAt(0) - 65) * 100
+    + parseInt(match[5] || '0');
+};
+
 cli.command('<version>', 'Get a changeset for specific version')
   .action((version, options) => (async () => {
     try {
@@ -15,6 +26,8 @@ cli.command('<version>', 'Get a changeset for specific version')
   })());
 
 cli.command('list', 'List changesets')
+  .option('--min <version>', 'Minimum version (included)')
+  .option('--max <version>', 'Maximum version (included)')
   .option('--json', 'Output in json format')
   .option('--all', 'List all changesets (alpha/beta included)')
   .option('--beta', 'List alpha/beta changesets')
@@ -25,6 +38,14 @@ cli.command('list', 'List changesets')
       : options.beta
         ? await scrapeBetaChangesets()
         : await scrapeArchivedChangesets();
+
+    // Filter by min/max.
+    var min = options.min ? toNumber(options.min) : Number.MIN_VALUE;
+    var max = options.max ? toNumber(options.max) : Number.MAX_VALUE;
+    results = results.filter(r => {
+      const n = toNumber(r.version);
+      return min <= n && n <= max;
+    });
 
     if (options.json) {
       if (options.versions)
