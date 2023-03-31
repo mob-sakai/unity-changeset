c9aa7a9357bcd806a8b74488d0b4bb0213c12206
mob-sakai <sakai861104@gmail.com>
2023-03-31 22:51:33 +0900

    fix: fix redirect url (unity3d.com to unity.com)

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,19 @@ export const UnityChangeset = UnityChangesetClass;
 export type UnityChangeset = UnityChangesetClass;
 
 const REGEXP_HUB_LINKS = /unityhub:\/\/\d{4}\.\d+\.\d+(a|b|f)\d+\/\w{12}/g;
-const UNITY_ARCHIVE_URL = "https://unity3d.com/get-unity/download/archive";
-const UNITY_ALPHA_URL = "https://unity3d.com/unity/alpha/";
-const UNITY_BETA_URL = "https://unity3d.com/unity/beta/";
+const UNITY_ARCHIVE_URL = "https://unity.com/releases/editor/archive";
 const UNITY_RSS_URL = "https://unity.com/releases/editor/releases.xml";
-const UNITY_BETA_RSS_URL = "https://unity3d.com/unity/beta/latest.xml";
+const UNITY_BETA_RSS_URL = "https://unity.com/releases/editor/beta/latest.xml";
 const UNITY_LTS_RSS_URL = "https://unity.com/releases/editor/lts-releases.xml";
+
+/*
+Unity release URLs for each lifecycle.
+*/
+const UNITY_RELEASE_URLS: { [key: string]: string } = {
+  "f": "https://unity.com/releases/editor/whats-new/",
+  "a": "https://unity.com/releases/editor/alpha/",
+  "b": "https://unity.com/releases/editor/beta/",
+};
 
 /*
  * Get an Unity changeset from specific Unity version.
@@ -19,22 +26,21 @@ const UNITY_LTS_RSS_URL = "https://unity.com/releases/editor/lts-releases.xml";
 export async function getUnityChangeset(
   version: string,
 ): Promise<UnityChangeset> {
-  const match = version.match(/^\d{4}\.\d+\.\d+(a|b|f)\d+$/);
-  const lifecycle = match?.[1] as string;
-  switch (lifecycle) {
-    case "f":
-      return (await scrapeArchivedChangesets())
-        .filter((c) => c.version === version)[0];
-    case "a":
-      return (await getUnityChangesetsFromUrl(UNITY_ALPHA_URL + version))
-        .filter((c) => c.version === version)[0];
-    case "b":
-      return (await getUnityChangesetsFromUrl(UNITY_BETA_URL + version))
-        .filter((c) => c.version === version)[0];
-    default:
-      throw Error(
-        `The given life-cycle '${lifecycle}' (in ${version}) was not supported`,
-      );
+  const match = version.match(/^(\d{4}\.\d+\.\d+)(a|b|f)\d+$/);
+  const lifecycle = match?.[2] as string;
+  const releaseUrl = UNITY_RELEASE_URLS[lifecycle];
+
+  if (lifecycle == "f") {
+    const shortVersion = match?.[1] as string;
+    const results = (await getUnityChangesetsFromUrl(releaseUrl + shortVersion))
+      .filter((c) => c.version === version);
+    if (0 < results.length) return results[0];
+
+    return (await scrapeArchivedChangesets())
+      .filter((c) => c.version === version)[0];
+  } else {
+    return getUnityChangesetsFromUrl(releaseUrl + version)
+      .then((results) => results.filter((c) => c.version === version)[0]);
   }
 }
 
